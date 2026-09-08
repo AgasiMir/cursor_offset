cb9539badc9687f55442dd0a1810eeacb491a351
Agasi Miroyan <limekick@mail.ru>
2026-09-08 17:46:01 +0300

    post cache invalidation

--- app/service/post_service.py
+++ app/service/post_service.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from uuid import UUID
 
+from app.init import redis_manager
+from app.middlewares.log import logger
 from app.schemas import (
     PostCreateSchema,
     PostPartialUpdateSchema,
@@ -44,7 +46,15 @@ class PostService:
     async def partial_update_post(
         self, post_id: UUID, post: PostPartialUpdateSchema
     ) -> PostReadSchema:
-        return await self.uow.posts.partial_update_post(post_id=post_id, post=post)
+        res = await self.uow.posts.partial_update_post(post_id=post_id, post=post)
+
+        # partial_update_post гарантированно возвращает PostReadSchema
+        # либо бросает PostNotFoundException, поэтому None тут невозможен.
+        pattern = f"fastapi-cache:post:{post_id}*"
+        deleted_count = await redis_manager.delete_by_pattern(pattern)
+        logger.info(f"Удалено ключей кэша для продукта {post_id}: {deleted_count}")
+
+        return res
 
     async def delete_post(self, post_id: UUID) -> dict:
         return await self.uow.posts.delete_post(post_id)
